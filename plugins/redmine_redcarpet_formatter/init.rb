e5134b76c714e4417c700ca26ea2f5935c826d31
yalab <rudeboyjet@gmail.com>
2012-10-20 14:21:41 +0900

    Install redmine_redcarpet_formatter plugin.

---
+++ plugins/redmine_redcarpet_formatter/init.rb
@@ -0,0 +1,16 @@
+# Redmine Redcarpet formatter
+
+require 'redmine'
+require 'redmine/wiki_formatting/markdown/formatter'
+require 'redmine/wiki_formatting/markdown/helper'
+
+
+Redmine::Plugin.register :redmine_redcarpet_formatter do
+  name 'Redcarpet Markdown Wiki formatter'
+  author 'Mikoto Misaka'
+  description 'Markdown wiki formatting by Redcarpet for Redmine'
+  version '2.0.1'
+
+  wiki_format_provider 'markdown', Redmine::WikiFormatting::Markdown::Formatter, Redmine::WikiFormatting::Markdown::Helper
+
+end
